0c2fc48211a9dceaf9cb215d1c91dc9dd47f210c
Frank Matranga <the.frankmatranga@gmail.com>
2015-07-24 17:20:46 +0000

    Improved teacher and course views

--- modules/sockets.js
+++ modules/sockets.js
@@ -13,50 +13,50 @@ module.exports = function(http) {
 
     try {
       var user = {name: client.firstName, username: client.username, code: client.code, tabs: 1};
-    }
-    catch(err) {
-      socket.emit('refresh');
-    }
-
-    socket.emit('pastMessages', {messages: messages});
-
-    console.log(codes.indexOf(user.code));
-
-    if(codes.indexOf(user.code) > -1){
-      console.log("New tab from "+user.username);
-      online[codes.indexOf(user.code)].tabs += 1;
-    }else{
-      codes.push(user.code);
-      online.push(user);
-      console.log("New connection from "+user.username);
-    }
-    console.log(codes);
-    console.log(online);
-
-    io.sockets.emit('online-list', {users: online});
-
-    socket.on('message', function (data) {
-      var d = {user: user, message: data.message, when: data.when};
-      messages.push(d);
-      io.sockets.emit('message', d);
-    });
-
-    socket.on('disconnect', function(socket) {
-      console.log("DISCONNECT from "+user.username);
-      var index = codes.indexOf(user.code);
-      console.log(index);
-      if(index == -1) throw "FAILED";
-      online[index].tabs -= 1;
-
-      if(online[index].tabs <= 0){
-        online.splice(index, 1);
-        codes.splice(index, 1);
+      
+  
+      socket.emit('pastMessages', {messages: messages});
+  
+      console.log(codes.indexOf(user.code));
+  
+      if(codes.indexOf(user.code) > -1){
+        console.log("New tab from "+user.username);
+        online[codes.indexOf(user.code)].tabs += 1;
+      }else{
+        codes.push(user.code);
+        online.push(user);
+        console.log("New connection from "+user.username);
       }
-
       console.log(codes);
       console.log(online);
+  
       io.sockets.emit('online-list', {users: online});
-    });
+  
+      socket.on('message', function (data) {
+        var d = {user: user, message: data.message, when: data.when};
+        messages.push(d);
+        io.sockets.emit('message', d);
+      });
+  
+      socket.on('disconnect', function(socket) {
+        console.log("DISCONNECT from "+user.username);
+        var index = codes.indexOf(user.code);
+        console.log(index);
+        if(index == -1) throw "FAILED";
+        online[index].tabs -= 1;
+  
+        if(online[index].tabs <= 0){
+          online.splice(index, 1);
+          codes.splice(index, 1);
+        }
+  
+        console.log(codes);
+        console.log(online);
+        io.sockets.emit('online-list', {users: online});
+      });
+    }catch(err) {
+      socket.emit('refresh');
+    }
   });
 
   return io;
