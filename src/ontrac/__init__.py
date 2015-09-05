bd00f990f94a61086ea4685536357f3af5f97526
Frank Matranga <thefrankmatranga@gmail.com>
2015-09-05 00:05:18 -0400

    Improved secrets system

--- src/ontrac/__init__.py
+++ src/ontrac/__init__.py
@@ -7,34 +7,33 @@ import scraper
 import cli
 import json
 
-VERSION = "0.0"
+VERSION = "0.1"
 PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),"..")) + "/secrets.json"
-
 
 class OnTrac:
     def __init__(self):
-        print "Initalizing OnTrac v"+VERSION
+        print "Initalized OnTrac v"+VERSION
 
-        path = self.get_path()
-
-        self.secrets = json.loads(open(path).read())
+        self.secrets = self.get_secrets()
         self.username = self.secrets['regis_username']
         self.password = self.secrets['regis_password']
         print "Found info for Regis student '"+self.username+"'"
-        if len(sys.argv) > 1 and sys.argv[1] == "cli":
-            self.exec_cli()
-        print "Done.\n"
 
-    def get_path(self):
+    def get_secrets(self):
         path = PATH
-        if os.path.isfile(PATH) == False:
-            if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]) == True:
+        secrets = json.loads(open(path).read())
+        if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]+"/secrets.json"):
+            try:
                 path = sys.argv[1]+"/secrets.json"
-            else:
-                path = raw_input("Please enter the path to secrets.json (global): ")+"/secrets.json"
-
+                secrets = json.loads(open(path).read())
+            except Exception:
+                try:
+                    secrets = json.loads(open(path).read())
+                except Exception:
+                    path = raw_input("Please enter the path to secrets.json (global): ")+"/secrets.json"
+                    secrets = json.loads(open(path).read())
         print "Using path '"+path+"' for secrets.json"
-        return path
+        return secrets
 
     def exec_cli(self):
         cli.main()
