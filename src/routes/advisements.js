bfd72f400e335ee2fe7df2d2c7b85ed3393d23a3
Frank Matranga <thefrankmatranga@gmail.com>
2015-09-09 22:37:49 -0400

    Fixed advisement list user view (closes #54)

--- src/routes/advisements.js
+++ src/routes/advisements.js
@@ -11,7 +11,16 @@ router.get('/', function(req, res){
       if(err){req.session.errs.push('An error occured, please try again.'); res.redirect(req.baseUrl); return;}
 
       if (advs){
+        advs.forEach(function(ad) {
+          var list = [];
+          ad.students.forEach(function(s) {
+            if(s.registered)
+              list.push("<span class='user-badge' data-username='"+s.username+"'>"+s.fullName+"</span>");
+          });
+          ad.list = list.join(', ');
+        });
         req.toJade.advisements = advs;
+
       }
       res.render('advisement/list', req.toJade);
     });
