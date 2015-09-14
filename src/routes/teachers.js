cee0a593d1d75a0c8c4c810a7f8a3bcdd434c3e4
Frank Matranga <thefrankmatranga@gmail.com>
2015-09-13 20:29:13 -0400

    Fixed teacher modals

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res, next) {
   var pageNum = (req.query.page ? parseInt(req.query.page) : 1);
 
   var sortBy = (req.query.sortBy ? req.query.sortBy : "");
-  req.Teacher.find({}, 'firstName lastName department mpicture ipicture')
+  req.Teacher.find({}, 'firstName username lastName department mpicture ipicture')
     .sort({sortBy: -1})
     .skip(perPage*(pageNum-1)).limit(perPage)
     .exec(function(err, teachers) {
