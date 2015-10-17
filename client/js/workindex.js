88a77a297890a1372fc1e32c0d5a933c6fc65fb1
Frank Matranga <thefrankmatranga@gmail.com>
2015-10-17 00:11:42 -0400

    Work index page accounts for no homework

--- client/js/workindex.js
+++ client/js/workindex.js
@@ -9,22 +9,26 @@ function workindex() {
       console.log(data);
       if(data.error){
         console.log(data.error);
-        $("#due-today").hide();
+        $("#due-today").remove();
         return;
       }
 
       if(data.success){
-        $("#due-today small").text(data.hwItems.length+" items");
-        var courses = [];
-        var total = data.hwItems.length;
-        var doneC = 0;
-        data.hwItems.forEach(function(item) {
-          if(item.completed)
-            doneC += 1;
-          if(courses.indexOf(item.course.title) == -1)
-            courses.push(item.course.title);
-        });
-        $("#due-today p").html("<a data-toggle='tooltip' title='"+courses.join(', ')+"' class='undecorated' href='/work/"+date+"'><b>"+data.hwItems.length+"</b> Homework items <b>"+Math.round((doneC/total)*1000)/10+"%</b> completed.</a>");
+        if(data.hwItems.length > 0){
+          $("#due-today small").text(data.hwItems.length+" items");
+          var courses = [];
+          var total = data.hwItems.length;
+          var doneC = 0;
+          data.hwItems.forEach(function(item) {
+            if(item.completed)
+              doneC += 1;
+            if(courses.indexOf(item.course.title) == -1)
+              courses.push(item.course.title);
+          });
+          $("#due-today p").html("<a data-toggle='tooltip' title='"+courses.join(', ')+"' class='undecorated' href='/work/"+date+"'><b>"+data.hwItems.length+"</b> Homework items <b>"+Math.round((doneC/total)*1000)/10+"%</b> completed.</a>");
+        }else{
+          $("#due-today").remove();
+        }
       }
     }
   });
@@ -35,22 +39,26 @@ function workindex() {
       console.log(data);
       if(data.error){
         console.log(data.error);
-        $("#closest").hide();
+        $("#closest").remove();
         return;
       }
 
       if(data.success){
-        $("#upcoming small").text(data.hwItems.length+" items");
-        var courses = [];
-        var total = data.hwItems.length;
-        var doneC = 0;
-        data.hwItems.forEach(function(item) {
-          if(item.completed)
-            doneC += 1;
-          if(courses.indexOf(item.course.title) == -1)
-            courses.push(item.course.title);
-        });
-        $("#upcoming p").html("<a data-toggle='tooltip' title='"+courses.join(', ')+"' class='undecorated' href='/work/"+$("#upcoming").data("closest")+"'><b>"+data.hwItems.length+"</b> Homework items <b>"+Math.round((doneC/total)*1000)/10+"%</b> completed.</a>");
+        if(data.hwItems.length > 0){
+          $("#upcoming small").text(data.hwItems.length+" items");
+          var courses = [];
+          var total = data.hwItems.length;
+          var doneC = 0;
+          data.hwItems.forEach(function(item) {
+            if(item.completed)
+              doneC += 1;
+            if(courses.indexOf(item.course.title) == -1)
+              courses.push(item.course.title);
+          });
+          $("#upcoming p").html("<a data-toggle='tooltip' title='"+courses.join(', ')+"' class='undecorated' href='/work/"+$("#upcoming").data("closest")+"'><b>"+data.hwItems.length+"</b> Homework items <b>"+Math.round((doneC/total)*1000)/10+"%</b> completed.</a>");
+        }else{
+          $("#closest").remove();
+        }
       }
     }
   });
