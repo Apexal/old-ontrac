346b8ac376d5d876d8e4fdc633f331709a0013a4
Frank Matranga <thefrankmatranga@gmail.com>
2015-11-07 16:08:56 -0500

    Switched from TRP to TRMS

--- server/scripts/clean.py
+++
@@ -1,9 +0,0 @@
-from trp import TRP
-
-# This clears the database of all extra info (not users, teachers, courses)
-t = TRP("server/", "192.168.1.145")
-print "Removed ", t.db.days.delete_many({}).deleted_count, "days"
-print "Removed ", t.db.hwitems.delete_many({}).deleted_count, "hwitems"
-print "Removed ", t.db.grades.delete_many({}).deleted_count, "grades"
-print "Removed ", t.db.reminders.delete_many({}).deleted_count, "reminders"
-t.exit()
