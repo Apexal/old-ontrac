fdb93251e2e5c6a7600468680073327a173de8ca
Frank Matranga <thefrankmatranga@gmail.com>
2015-11-29 16:21:43 -0500

    Fixed login client js error bug

--- client/js/login.js
+++ client/js/login.js
@@ -4,44 +4,45 @@ function login() {
   var login = function() {
     var username = $("#username").val();
     var password = $("#password").val();
-    if(!username || !password)
+    if (!username || !password)
       return;
-
+    
+    $("#login-errors .alert").remove();
     $("#login-button").text("Logging in...");
     $("body").css("cursor", "wait");
-    if(loggingIn == false){
+    if (loggingIn == false) {
       loggingIn = true;
 
       $.ajax({
         type: "POST",
         url: "/login",
         data: {
-            username: username,
-            password: password
+          username: username,
+          password: password
         },
         success: function(data) {
-            $("#login-errors .alert").remove();
-            if (data.errors) {
-                for (var err in data.errors) {
-                    $("#login-errors").append("<div class='alert alert-danger'>"+
-                      data.errors[err]+"</div>");
-                }
-                $("#login-button").text("Login");
-                $("body").css("cursor", "auto");
-                loggingIn = false;
-            } else {
-                window.location.href = $("#login-form").data(
-                    "redirect");
-            }
-        },
-        error: function(XMLHttpRequest, textStatus, error){
           $("#login-errors .alert").remove();
-          console.log(textStatus +": "+error);
+          if (data.errors) {
+            data.errors.forEach(function(error) {
+              $("#login-errors").append("<div class='alert alert-danger'>" +
+                error + "</div>");
+            });
+            $("#login-button").text("Login");
+            $("body").css("cursor", "auto");
+            loggingIn = false;
+          } else {
+            window.location.href = $("#login-form").data(
+              "redirect");
+          }
+        },
+        error: function(XMLHttpRequest, textStatus, error) {
+          $("#login-errors .alert").remove();
+          console.log(textStatus + ": " + error);
           $("#login-button").text("Login");
           $("body").css("cursor", "auto");
           loggingIn = false;
 
-          $("#login-errors").append("<div class='alert alert-danger'>"+
+          $("#login-errors").append("<div class='alert alert-danger'>" +
             "There is an issue with the server! Please tell Frank!</div>");
         }
       });
@@ -49,7 +50,7 @@ function login() {
   };
 
   $("#password").keypress(function(e) {
-    if(e.which == 13) {
+    if (e.which == 13) {
       login();
     }
   });
