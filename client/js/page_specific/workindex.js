bac2fb5d0d3d2abd4d1ab4aa62196055b98f4c4d
Frank Matranga <thefrankmatranga@gmail.com>
2015-12-07 19:45:03 -0500

    GREATLY improved work index JS

---
+++ client/js/page_specific/workindex.js
@@ -0,0 +1,84 @@
+/* WORK INDEX PAGE FUNCTIONALITY
+ * - Today's work due section
+ * - Next day's work due section
+ * - Navigation calendar
+ */
+
+function workindex() {
+  $("#hw-calendar").fullCalendar({
+    weekends: false,
+    header: {
+			left: 'prev,next today',
+			center: 'title',
+      right: ''
+		},
+    dayClick: function(date, jsEvent, view) {
+      window.location.href = "/work/"+date.format("YYYY-MM-DD");
+    },
+    dayRender: function( date, cell ) {
+      if(date.format("YYYY-MM-DD") == $("#upcoming").data("closest")){
+        cell.css("background-color", "#64e6fc");
+      }
+    },
+    defaultView: 'month'
+  });
+
+  function createWorkSection(dateString, holder){
+    $.get("/homework/"+dateString, function(data){
+      var date = moment(dateString, "YYYY-MM-DD");
+      if(data){
+
+        if(data.error){
+          console.log(data.error);
+          holder.remove();
+          return;
+        }
+
+        if(data.length > 0){
+          holder.find("small").text(data.length+" items");
+          var courses = [];
+          var total = data.length;
+          var doneC = 0;
+          data.forEach(function(item){
+            if(item.completed)
+              doneC += 1;
+            if(courses.indexOf(item.course.title) == -1)
+              courses.push(item.course.title);
+          });
+
+          var table = holder.find("table");
+
+          var hw = {};
+          var hwTitles = [];
+          var doneC = 0;
+          var total = data.length;
+          data.forEach(function(item){
+            if(item.completed)
+              doneC++;
+            //console.log(item.course.title);
+            if(hwTitles.indexOf(item.course.title) == -1)
+              hwTitles.push(item.course.title);
+
+            if(!hw[item.course.title])
+              hw[item.course.title] = [];
+
+            hw[item.course.title].push(item);
+          });
+
+          table.html("");
+          var html = "";
+          hwTitles.forEach(function(cTitle) {
+            html+="<tr><td>"+cTitle+"</td><td>"+hw[cTitle].length+" items</td></tr>";
+          });
+          table.html(html);
+          holder.find("p").html("<a data-toggle='tooltip' title='"+courses.join(', ')+"' class='undecorated' href='/work/"+dateString+"'><b>"+data.length+"</b> Homework items for <b>"+date.format("dddd [the] Do")+"</b><br><span class='text-muted'><b>"+Math.round((doneC/total)*1000)/10+"%</b> completed.</span></a>");
+        }else{
+          holder.find("p").html("<a data-toggle='tooltip' class='undecorated' href='/work/"+dateString+"'><span class='text-muted'>You have not yet recorded any homework items due <b>"+date.format("dddd [the] Do")+"</b>.</span></a>");
+        }
+      }
+    });
+  }
+
+  createWorkSection(moment().format("YYYY-MM-DD"), $("#due-today"));
+  createWorkSection($("#upcoming").data("closest"), $("#upcoming"));
+}
