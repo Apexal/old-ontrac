1ece529e02ad09b8232b2005d88b67315f29577c
Frank Matranga <thefrankmatranga@gmail.com>
2015-12-16 21:38:27 -0500

    Microsoft Edge warning (Untested)

--- client/js/lib.js
+++ client/js/lib.js
@@ -1,3 +1,8 @@
+if(navigator.userAgent.indexOf('Edge') > -1){
+  // in Edge the navigator.appVersion does not say trident
+  alert("Unfortunately, OnTrac does not yet work on Microsoft Edge. I am looking into this. Please use Chrome or Firefox for now.");
+}
+
 (function(){
   PNotify.desktop.permission();
   page = window.location.pathname;
